d450cf1a85cfcd30e4d5171f4c3fe54915648823
PolyRibina <poly.ribina@gmail.com>
2020-05-31 13:20:34 +0300

    Изменения в коде для проверки

--- Тест/UnitTest1.cs
+++ Тест/UnitTest1.cs
@@ -14,7 +14,7 @@ namespace Тест
             double[] expResult = new double[0];
             int a = 2, b = 6, c = 9;
             double[] actResult = Class1.Mathing(a, b,c,out actError);
-            Assert.AreEqual(expError, actError, "Полученное сообщение ошибке("+actError+") не совпадает с ожидаемым" + expError);
+            Assert.AreEqual(expError, actError, "Полученное сообщение ошибке ("+actError+") не совпадает с ожидаемым" + expError);
             Assert.AreEqual(expResult.Length, actResult.Length);
             for (int i = 0; i< expResult.Length; i++)
             {
